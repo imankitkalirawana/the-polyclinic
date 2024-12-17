17902a9ec0034b13c2898549ac9016b2e50b4313
Your Name <imankitkalirawana@gmail.com>
2024-12-17 18:20:39 +0530

    Refactor dashboard loading component to use skeleton component

---
+++ app/dashboard/drugs/[did]/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardDetailsSkeleton from '@/components/skeletons/dashboard-details-skeleton';
+
+export default function Loading() {
+  return <DashboardDetailsSkeleton />;
+}
--- app/dashboard/services/page.tsx
+++ app/dashboard/services/page.tsx
@@ -3,7 +3,6 @@ import { API_BASE_URL } from '@/lib/config';
 import { ServiceType } from '@/models/Service';
 import axios from 'axios';
 import { cookies } from 'next/headers';
-import { Suspense } from 'react';
 
 async function getData() {
   try {
