ae025c17bf895024fa2946f6eb95dee7f26bac53
Your Name <imankitkalirawana@gmail.com>
2024-12-20 23:16:37 +0530

    Refactor package.json to add dns dependency

--- app/api/cron/30m/route.ts
+++ app/api/cron/30m/route.ts
@@ -4,8 +4,9 @@ import Appointment from '@/models/Appointment';
 import { connectDB } from '@/lib/db';
 import { format } from 'date-fns';
 import { OverdueEmail } from '@/utils/email-template/overdue-email';
+import { checkDomainMx, transporter } from '@/lib/nodemailer';
 
-export const POST = async function POST(request: any, context: any) {
+export const POST = async function POST(request: any) {
   try {
     await connectDB();
     const appointments = await Appointment.find({
@@ -16,18 +17,24 @@ export const POST = async function POST(request: any, context: any) {
     for (const appointment of appointments) {
       await Appointment.findOneAndUpdate(
         { aid: appointment.aid },
-        { status: 'overdue' },
+        {},
         { new: true }
       ).then(async () => {
-        await sendHTMLMail(
-          appointment.email,
-          'Action Needed: Reschedule Your Missed Appointment',
-          OverdueEmail(
-            appointment.aid,
-            appointment.name,
-            format(appointment.date, 'PPPPp')
-          )
-        );
+        await checkDomainMx('contact@divinely.dev')
+          .then(async () => {
+            await sendHTMLMail(
+              appointment.email,
+              'Action Needed: Reschedule Your Missed Appointment',
+              OverdueEmail(
+                appointment.aid,
+                appointment.name,
+                format(appointment.date, 'PPPPp')
+              )
+            );
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       });
     }
 
--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import dns from 'dns';
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 
@@ -11,3 +12,18 @@ export const transporter = nodemailer.createTransport({
     pass: password
   }
 });
+
+export function checkDomainMx(email: string) {
+  return new Promise((resolve, reject) => {
+    const domain = email.split('@')[1];
+    dns.resolveMx(domain, (err, addresses) => {
+      if (err) {
+        reject('Invalid domain');
+      } else if (addresses && addresses.length > 0) {
+        resolve('Valid domain with MX records');
+      } else {
+        reject('No MX records found');
+      }
+    });
+  });
+}
