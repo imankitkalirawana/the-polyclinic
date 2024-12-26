8392e2046ebc88ba61f90da8344fa5c33d709411
Your Name <imankitkalirawana@gmail.com>
2024-12-26 08:50:43 +0530

    Refactor layout component to limit the maximum height of the user profile section

--- components/dashboard/layout.tsx
+++ components/dashboard/layout.tsx
@@ -57,7 +57,7 @@ export default function DashboardLayout({
           //     backgroundSize: 'cover',
           //     backgroundPosition: 'start'
           //   }}
-          className="relative aspect-square w-full"
+          className="relative aspect-square max-h-56 w-full"
         >
           <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
           <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center gap-8 p-4">
