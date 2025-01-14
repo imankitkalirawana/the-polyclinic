1be9bafe1827b15d99742c68444eeb9d8361210e
Your Name <imankitkalirawana@gmail.com>
2025-01-14 15:47:22 +0530

    Refactor route.ts to remove authorization check in GET request

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,9 +2,6 @@ import { NextResponse } from 'next/server';
 
 export const GET = async function GET(request: any) {
   try {
-    if (request.auth?.user?.role !== 'admin') {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    }
     return NextResponse.json({ message: 'GET request' });
   } catch (error: any) {
     console.error(error);
