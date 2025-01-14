8d8f9772b561c332c0fab079c3eb71623f98ecbf
Your Name <imankitkalirawana@gmail.com>
2025-01-14 15:48:08 +0530

    Refactor route.ts to handle POST requests and implement error handling

--- app/api/route.ts
+++ app/api/route.ts
@@ -8,3 +8,13 @@ export const GET = async function GET(request: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 };
+export const POST = async function POST(request: any, context: any) {
+  try {
+    const body = await request.json();
+    console.log('body', body);
+    return NextResponse.json({ message: 'POST request', body });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+};
