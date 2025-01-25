112cc88e0b6cdc6696eb49bd0053730652da4391
Your Name <009ankitkalirawana@gmail.com>
2025-01-25 13:49:39 +0530

    Refactor import statements to use single quotes in skeleton.tsx and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-import { heroui } from "@heroui/react";
+import { heroui } from '@heroui/react';
 
 const config = {
   content: [
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
