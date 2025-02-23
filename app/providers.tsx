0cb1bd61bc3b0918e0317dbb2a6a8732f22e5ea2
Bhuneshvar <009ankitkalirawana@gmail.com>
2025-02-23 12:39:44 +0530

    Add ToastProvider and update router navigation in providers configuration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { HeroUIProvider } from '@heroui/react';
+import { ToastProvider } from '@heroui/toast';
+
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '@/store';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useRouter } from 'nextjs-toploader/app';
 
 declare module '@react-types/shared' {
   interface RouterConfig {
@@ -22,14 +24,19 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000
       }
     }
   });
   return (
     <HeroUIProvider navigate={router.push}>
+      <ToastProvider
+        toastProps={{
+          variant: 'flat',
+          radius: 'lg'
+        }}
+      />
+
       <QueryClientProvider client={queryClient}>
         <SessionProvider>
           <ReduxProvider store={store}>{children}</ReduxProvider>
