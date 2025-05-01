3201123566d25470c118687f00670775666392bb
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-05-01 23:10:46 +0530

    refactor(dashboard): rename DashboardFunc to Dashboard for consistency

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export default function DashboardFunc() {
-  return <>hello</>;
-=======
 export default function Dashboard() {
   return <>Hello</>;
->>>>>>> 431d031b5d61ac18a122f95b4c8c782b9e1084dd
 }
