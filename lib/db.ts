f7e55566642cd20757dc921c11c5fef93294977b
Ankit Kalirawana <imankitkalirawana@gmail.com>
2025-06-02 20:01:54 +0530

    feat(sentry): integrate Sentry for error tracking and monitoring in Next.js app

--- lib/db.ts
+++ lib/db.ts
@@ -7,7 +7,6 @@ const uri = process.env.MONGODB_URI || '';
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
-    console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
