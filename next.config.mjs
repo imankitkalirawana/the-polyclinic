88f68816bdc06de0d99d7ef5b461a5b0c7a6defa
Ankit Kalirawana <imankitkalirawana@gmail.com>
2025-06-03 17:44:38 +0530

    chore(config): disable telemetry and update secrets in node.js.yml

--- next.config.mjs
+++ next.config.mjs
@@ -37,4 +37,5 @@ export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
+  telemetry: false,
 });
