a2f79fcd2155063007a686f564674a19c2aac1e4
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-06-05 14:54:03 +0530

    refactor(dashboard): reorganize loading skeleton component and update imports

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import { auth } from '@/auth';
+import DashboardTableSkeleton from '@/components/skeletons/dashboard/dashboard-table-skeleton';
 
 export default async function Dashboard() {
   const session = await auth();
 
-  return <>Hello World</>;
+  return (
+    <>
+      <DashboardTableSkeleton />
+    </>
+  );
 }
