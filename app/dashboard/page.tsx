3146c4f97316e315136acfaf978148c99b6fed94
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-06-05 14:55:55 +0530

    refactor(dashboard): remove loading skeleton and return a simple greeting

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,7 @@
 import { auth } from '@/auth';
-import DashboardTableSkeleton from '@/components/skeletons/dashboard/dashboard-table-skeleton';
 
 export default async function Dashboard() {
   const session = await auth();
 
-  return (
-    <>
-      <DashboardTableSkeleton />
-    </>
-  );
+  return <>Hello World</>;
 }
