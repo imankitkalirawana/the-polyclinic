9e338e47c3e73175c87e199a5f3b41e7c6518946
Ankit Kalirawana <imankitkalirawana@gmail.com>
2025-07-11 23:47:19 +0530

    refactor(tsconfig,layout): simplify formatting and structure in tsconfig and layout files

--- app/dashboard/users/[uid]/layout.tsx
+++ app/dashboard/users/[uid]/layout.tsx
@@ -2,17 +2,13 @@ import { HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { getUserWithUID } from '@/services/api/user';
 
-export default async function Layout(
-  props: {
-    params: Promise<{ uid: number }>;
-    children: React.ReactNode;
-  }
-) {
+export default async function Layout(props: {
+  params: Promise<{ uid: number }>;
+  children: React.ReactNode;
+}) {
   const params = await props.params;
 
-  const {
-    children
-  } = props;
+  const { children } = props;
 
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
