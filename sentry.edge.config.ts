bf9a7572dd6822b1081494134b395cf57b71c901
Ankit Kalirawana <imankitkalirawana@gmail.com>
2025-07-24 00:04:05 +0530

    chore(sentry): replace hardcoded DSN with environment variable for Sentry configuration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 
 if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
-    dsn: 'https://8677dec65dd0d5ba201a557e240ec0e5@o4509429537767424.ingest.us.sentry.io/4509429541502976',
+    dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1,
     debug: false,
   });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
-    dsn: 'https://8677dec65dd0d5ba201a557e240ec0e5@o4509429537767424.ingest.us.sentry.io/4509429541502976',
+    dsn: process.env.SENTRY_DSN,
 
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
     tracesSampleRate: 1,
