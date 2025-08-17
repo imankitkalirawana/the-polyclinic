1a933a9ba1945652da64eccbf525ae2fab9424d8
Bhuneshvar <imankitkalirawana@gmail.com>
2025-08-17 10:28:24 +0530

    feat(appointments): implement new patient creation form with validation and improved UI

---
+++ types/patient.ts
@@ -0,0 +1,12 @@
+export interface NewPatientFormValues {
+  name: string;
+  email: string;
+  phone: string;
+  gender: string;
+  dob: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  zipcode: string;
+}
