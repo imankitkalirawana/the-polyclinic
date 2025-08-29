ce947cd40005686ed158de13f889f79aba01c4ff
Bhuneshvar <imankitkalirawana@gmail.com>
2025-08-30 01:05:53 +0530

    feat(permissions): add appointment permissions and remove test permissions

--- lib/permissions/api/index.ts
+++ lib/permissions/api/index.ts
@@ -1,11 +1,11 @@
 // lib/permissions/index.ts
 import { basePermissions } from './base';
 import { organizationPermissions } from './organization';
-import { testPermissions } from './test';
+import { appointmentPermissions } from './client/appointment';
 import { PermissionConfig } from './types';
 
 export const permissionsConfig: PermissionConfig = {
   ...basePermissions,
   ...organizationPermissions,
-  ...testPermissions,
+  ...appointmentPermissions,
 };
