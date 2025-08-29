608db76b24f52d7ba9aefd04491f861235590fe0
Bhuneshvar <imankitkalirawana@gmail.com>
2025-08-30 02:26:53 +0530

    refactor(appointments): remove unused appointment types and modes; clean up imports

--- types/client/appointment.ts
+++ types/client/appointment.ts
@@ -1,51 +1,10 @@
 // TODO: Remove this file after moving to @/services
 import { ButtonProps } from '@heroui/react';
 
-import type { $FixMe, Gender } from '..';
+import type { $FixMe } from '..';
 
 import { OrganizationUserType } from '../system/organization';
 import { AppointmentType } from '@/services/client/appointment';
-
-export const appointmentTypes = [
-  {
-    label: 'Consultation',
-    value: 'consultation',
-    description:
-      'A consultation is a visit to a doctor for a general check-up or to discuss a specific health concern.',
-  },
-  {
-    label: 'Follow-up',
-    value: 'follow-up',
-    description:
-      'A follow-up is a visit to a doctor to check on the progress of a specific health concern.',
-  },
-  {
-    label: 'Emergency',
-    value: 'emergency',
-    description: 'An emergency is a visit to a doctor for a sudden and urgent health concern.',
-  },
-] as const;
-
-export const appointmentModes = ['online', 'offline'] as const;
-
-export interface PatientInfo {
-  uid: number;
-  name: string;
-  phone?: string;
-  email: string;
-  gender?: Gender;
-  age?: number;
-  image?: string;
-}
-
-export interface DoctorInfo {
-  uid: number;
-  name: string;
-  email: string;
-  phone: string;
-  seating?: string;
-  image?: string;
-}
 
 export type ActionType =
   | 'reschedule'
