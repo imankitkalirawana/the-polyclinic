bf2e14fb1365962305ff75735803c7d37b117ae2
Bhuneshvar <imankitkalirawana@gmail.com>
2025-12-20 20:22:44 +0530

    refactor(dashboard): remove session validation from NewService page and simplify component structure; add Medicines component for appointments queue

---
+++ components/client/appointments/queue/medicines.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+const columns = [
+  {
+    name: 'Medicine',
+    id: 'medicine',
+  },
+  {
+    name: 'Quantity',
+    id: 'quantity',
+  },
+  {
+    name: 'Frequency',
+    id: 'frequency',
+  },
+  {
+    name: 'Notes',
+    id: 'notes',
+  },
+];
+
+export default function Medicines() {
+  return <div>Medicines</div>;
+}
