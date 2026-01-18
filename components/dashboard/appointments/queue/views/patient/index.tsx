175eb0197126aa748be2fa02be88ea182d08526d
Bhuneshvar <imankitkalirawana@gmail.com>
2026-01-18 21:53:43 +0530

    feat(dashboard): enhance PatientQueueView with tabbed layout for upcoming and previous appointments, including appointment count display

--- components/dashboard/appointments/queue/views/patient/index.tsx
+++ components/dashboard/appointments/queue/views/patient/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useAllAppointmentQueues } from '@/services/client/appointment/queue/queue.query';
-import { Card } from '@heroui/react';
+import { Card, Chip, Tab, Tabs } from '@heroui/react';
 
 export default function PatientQueueView() {
   const { data: appointments } = useAllAppointmentQueues();
@@ -8,13 +8,46 @@ export default function PatientQueueView() {
   return (
     <div className="p-4">
       <h1>My appointments</h1>
-      <div className="grid grid-cols-[repeat(auto-fill,400px)] gap-2">
-        {appointments?.map((appointment) => (
-          <Card key={appointment.id} isPressable>
-            <p>{appointment.sequenceNumber}</p>
-          </Card>
-        ))}
-      </div>
+      <Tabs aria-label="My appointments">
+        <Tab
+          key="upcoming"
+          title={
+            <div className="flex items-center gap-2">
+              <span>Upcoming</span>
+              <Chip size="sm" variant="flat">
+                {appointments?.length}
+              </Chip>
+            </div>
+          }
+        >
+          <div className="grid grid-cols-3 gap-2">
+            {appointments?.map((appointment) => (
+              <Card key={appointment.id} isPressable>
+                <p>{appointment.sequenceNumber}</p>
+              </Card>
+            ))}
+          </div>
+        </Tab>
+        <Tab
+          key="previous"
+          title={
+            <div className="flex items-center gap-2">
+              <span>Previous</span>
+              <Chip size="sm" variant="flat">
+                {appointments?.length}
+              </Chip>
+            </div>
+          }
+        >
+          <div className="grid grid-cols-3 gap-2">
+            {appointments?.map((appointment) => (
+              <Card key={appointment.id} isPressable>
+                <p>{appointment.sequenceNumber}</p>
+              </Card>
+            ))}
+          </div>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
