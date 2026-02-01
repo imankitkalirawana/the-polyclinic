4b5d2790d0f6919e24fbbe9bf3fba3469e3d0b77
Bhuneshvar <imankitkalirawana@gmail.com>
2026-02-01 19:54:11 +0530

    refactor(auth): remove cookie handling from getServerSession to streamline session retrieval

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,13 +1,8 @@
-import { cookies } from 'next/headers';
 import { Session } from '@/types/session';
 import { cache } from 'react';
 import { AuthApi } from '@/services/common/auth/auth.api';
 
 export const getServerSession = cache(async (): Promise<Session | null> => {
-  const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get('connect.sid')?.value;
-  if (!sessionCookie) return null;
-
   try {
     const res = await AuthApi.getSession();
 
